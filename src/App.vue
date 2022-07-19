858e9d801a75c90d2ff37178fee85da5721f7d28
Maciej Koczot <mefiuea@gmail.com>
2022-07-19 18:31:23 +0200

    upgrade results. Add views to display score

--- src/App.vue
+++ src/App.vue
@@ -14,58 +14,81 @@
     <div class="flex w-full h-screen justify-center items-center">
       <div class="w-full max-w-xl">
         <h1 class="font-bold text-5xl text-center text-indigo-700">Quiz game</h1>
-        <!--        question and answer container-->
+        <!--        question and answer and results container-->
         <div class="bg-white p-12 rounded-lg shadow-lg w-full mt-8">
-          <p v-if="questions.length > 0" class="text-2xl font-bold">{{ questions[questionIndex - 1]['question'] }}</p>
-          <div
-              v-for="answer in questions[questionIndex-1]['choices']"
-              class="block mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg option-default answer-option"
-              v-bind:key="answer"
-              v-on:click="selectedAnswerFunction($event)"
-              v-on:mouseover="hoverClassOn($event)"
-              v-on:mouseout="hoverClassOff($event)"
-              :class="{'option-selected': selectedAnswersDictionary[questionIndex] === answer}"
-          >
-            {{ answer }}
-          </div>
-          <!--progress status-->
-          <div class="mt-8 text-center">
-            <div class="flex justify-center items-center">
-              <span class="text-black text-xs">Saved questions</span>
+          <!--display div with questions-->
+          <div v-if="questionIndex-1 < questionCounter">
+            <p v-if="questions.length > 0" class="text-2xl font-bold">{{ questions[questionIndex - 1]['question'] }}</p>
+            <div
+                v-for="answer in questions[questionIndex-1]['choices']"
+                class="block mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg option-default answer-option"
+                v-bind:key="answer"
+                v-on:click="selectedAnswerFunction($event)"
+                v-on:mouseover="hoverClassOn($event)"
+                v-on:mouseout="hoverClassOff($event)"
+                :class="{'option-selected': selectedAnswersDictionary[questionIndex] === answer}"
+            >
+              {{ answer }}
             </div>
-            <div class="mb-4 w-full h-1 bg-gray-200 rounded-full dark:bg-gray-700">
-              <div class="h-1 bg-yellow-500 font-medium text-blue-100 rounded-full" :style="{width: widthProgress + '%'}"></div>
+            <!--progress status-->
+            <div class="mt-8 text-center">
+              <div class="flex justify-center items-center">
+                <span class="text-black text-xs">Saved questions</span>
+              </div>
+              <div class="mb-4 w-full h-1 bg-gray-200 rounded-full dark:bg-gray-700">
+                <div class="h-1 bg-yellow-500 font-medium text-blue-100 rounded-full"
+                     :style="{width: widthProgress + '%'}"></div>
+              </div>
+              <p>Question: {{ questionIndex }}/{{ questionCounter }}</p>
             </div>
-            <p>Question: {{ questionIndex }}/{{ questionCounter }}</p>
+            <!--next question button-->
+            <div class="mt-6 flow-root">
+              <button
+                  class="float-right px-5 py-2 myButton"
+                  v-show="questionIndex - 1 < questionCounter - 1"
+                  :disabled="selectedAnswer === undefined || selectedAnswer === ''"
+                  v-on:click="nextQuestion()"
+              >
+                Next &gt;
+              </button>
+              <!--previous question button-->
+              <button
+                  class="float-left px-5 py-2 myButton"
+                  v-show="questionIndex - 1 >= 0"
+                  :disabled="questionIndex - 1 === 0"
+                  v-on:click="previousQuestion()"
+              >
+                &lt; Previous
+              </button>
+              <!--finish quiz button-->
+              <button
+                  class="float-right px-5 py-2 myButton"
+                  v-show="questionIndex - 1 === questionCounter - 1"
+                  :disabled="selectedAnswer === undefined || selectedAnswer === ''"
+                  v-on:click="showResults()"
+              >
+                Finish
+              </button>
+            </div>
           </div>
-          <!--next question button-->
-          <div class="mt-6 flow-root">
-            <button
-                class="float-right px-5 py-2 myButton"
-                v-show="questionIndex - 1 < questionCounter - 1"
-                :disabled="selectedAnswer === undefined || selectedAnswer === ''"
-                v-on:click="nextQuestion()"
-            >
-              Next &gt;
-            </button>
-            <!--previous question button-->
-            <button
-                class="float-left px-5 py-2 myButton"
-                v-show="questionIndex - 1 >= 0"
-                :disabled="questionIndex - 1 === 0"
-                v-on:click="previousQuestion()"
-            >
-              &lt; Previous
-            </button>
-            <!--finish quiz button-->
-            <button
-                class="float-right px-5 py-2 myButton"
-                v-show="questionIndex - 1 === questionCounter - 1"
-                :disabled="selectedAnswer === undefined || selectedAnswer === ''"
-                v-on:click="showResults()"
-            >
-              Finish
-            </button>
+          <!--display div with results-->
+          <!--                    <div v-else>-->
+          <br>
+          <hr>
+          <br>
+          <div>
+
+            <div class="font-bold text-5xl text-center mb-10">Results:</div>
+            <div class="font-bold text-2xl text-center mb-10">Correct answers: {{ this.correctAnswers }}/{{ this.questionCounter }}</div>
+            <div class="mb-16" v-for="(question, idx) in questions" v-bind:key="question">
+              <p class="font-bold text-3xl text-center bg-red-200 bg-opacity-50 rounded-full p-2 m-2"
+                 v-bind:class="{'bg-green-200 bg-opacity-50 rounded-full p-2 m-2': question.correctAnswer === selectedAnswersDictionary[idx + 1]}">
+                {{ question.question }}</p>
+              <p class="border-yellow-500 border-2 rounded-full p-2 m-2 font-bold">Your answer: <span
+                  class="font-light text-2xl">{{ selectedAnswersDictionary[idx + 1] }}</span></p>
+              <p class="border-green-500 border-2 rounded-full p-2 m-2 font-bold">Correct answer: <span
+                  class="font-light text-2xl">{{ question.correctAnswer }}</span></p>
+            </div>
           </div>
         </div>
       </div>
@@ -79,12 +102,14 @@ export default {
   name: "App",
   data() {
     return {
-      isHovering: false,
-      selectedAnswer: '',
-      selectedAnswersDictionary: {},
-      questionIndex: 1,
-      questionCounter: 3,
-      widthProgress: 0,
+      isHovering: false, // variable to indicate if user has hovered the mouse over the answer
+      selectedAnswer: '',  // variable to store actual selected answer
+      selectedAnswersDictionary: {}, // dictionary to collect all answers from user depending on question number
+      questionIndex: 1, // actual question number
+      questionCounter: 3,  // total number of questions
+      widthProgress: 0,  // progress bar width - using in style
+      correctAnswers: 0, // counter for correct answers
+      wrongAnswers: 0,  // counter for wrong answers
       questions: [
         {
           question: 'question 1 bla bla',
@@ -172,15 +197,26 @@ export default {
       this.updateProgressBar()
     },
     showResults() {
-
+      this.questionIndex++;
+      console.log('RESULTS: ')
+      this.info();
+      for (let i = 0; i < this.questions.length; i++) {
+        if (this.questions[i].correctAnswer === this.selectedAnswersDictionary[i+1]) {
+          this.correctAnswers++
+        }
+        else {
+          this.wrongAnswers++
+        }
+      }
+      console.log('Correct answers = ', this.correctAnswers)
+      console.log('Wrong answers = ', this.wrongAnswers)
     },
     // static methods
     updateProgressBar() {
       // console.log(Object.keys(this.selectedAnswersDictionary).length)
       // update progress bar
-      this.widthProgress = (Object.keys(this.selectedAnswersDictionary).length / this.questionCounter) * 100
-      // this.widthProgress = (this.questionIndex / this.questionCounter) * 100
-      console.log(this.widthProgress)
+      this.widthProgress = (Object.keys(this.selectedAnswersDictionary).length / this.questionCounter) * 100;
+      console.log(this.widthProgress);
     }
   },
   beforeMount() {
