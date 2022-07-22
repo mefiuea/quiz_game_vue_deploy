d859b3fc7391ce460a9d38869acd708841a44d7a
Maciej Koczot <mefiuea@gmail.com>
2022-07-22 12:22:01 +0200

    add documentation and comments

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <body>
-  <!--container with START button-->
+  <!--START view-->
   <div class="flex w-full h-screen justify-center items-center" v-if="isStart === false">
     <div class="w-full max-w-xl text-center">
       <div class="flex-1 bg-white p-12 rounded-lg shadow-lg w-full mt-8 justify-center items-center">
@@ -36,7 +36,7 @@
     </div>
   </div>
 
-  <!--container with questions-->
+  <!--questions view-->
   <div class="flex w-full h-screen justify-center items-center" v-if="isStart === true && isFinish === false">
     <div class="w-full max-w-xl">
       <h1 class="font-bold text-5xl text-center quiz-text">Quiz game</h1>
@@ -121,11 +121,12 @@
         <div class="font-bold text-xl text-center mb-10 mt-5">Total time: {{ this.timerGlobalDelta }} [s]</div>
         <!--results - questions-->
         <div class="mb-16 relative" v-for="(question, idx) in questions" v-bind:key="question">
-          <div class="bg-blue-400 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold text-center absolute right-0 -top-8 shadow-2xl question">
-            <p class="transform -rotate-45">q{{ idx+1 }}</p>
+          <div class="bg-blue-400 p-1 transform rotate-45 rounded-md h-10 w-10 text-white font-bold text-center
+          absolute right-0 -top-8 shadow-2xl question">
+            <p class="transform -rotate-45">q{{ idx + 1 }}</p>
           </div>
           <p class="font-bold text-3xl text-center bg-red-200 bg-opacity-50 rounded p-2 m-2"
-             v-bind:class="{'bg-green-200 bg-opacity-50 rounded-full p-2 m-2': question.correctAnswer === selectedAnswersDictionary[idx + 1]}"
+             v-bind:class="{'bg-green-200 bg-opacity-50 rounded p-2 m-2': question.correctAnswer === selectedAnswersDictionary[idx + 1]}"
              v-html="question.question"
           ></p>
           <p class="border-yellow-500 border-2 rounded-full p-2 m-2 font-bold">Your answer: <span
@@ -171,21 +172,22 @@ export default {
     }
   },
   methods: {
-    info() {
-      console.log('*********************************')
-      console.log('selectedAnswer = ', this.selectedAnswer)
-      console.log('selectedAnswersDictionary = ', this.selectedAnswersDictionary)
-      console.log('questionIndex = ', this.questionIndex)
-      console.log('*********************************')
-    },
     fetchData() {
-      // creating query string
+      /**
+       * Function to get data from external api.
+       * In addition, setting the launch variables.
+       * Started when the START button is pressed.
+       */
+
+          // creating query string
       let params = new URLSearchParams();
       params.append('amount', this.selectedNumberOfQuestions.toString())
       const apiUrl = 'https://opentdb.com/api.php?' + params.toString();
 
+      // this variable change view to loading view on START page
       this.isLoading = true
 
+      // send request to external api. Catch errors when it is not possible to get data from external api.
       const request = async () => {
         try {
           const response = await fetch(apiUrl).catch(function (error) {
@@ -207,6 +209,12 @@ export default {
 
     },
     prepareQuestions(data) {
+      /**
+       * Function called when data from external api has been successfully retrieved.
+       * In addition, setting the launch variables.
+       * Function call inside fetchData().
+       */
+
       // copy data from external api to my dictionary 'questions'
       for (let i = 0; i < data.results.length; i++) {
         const dict = {
@@ -219,12 +227,12 @@ export default {
         this.questions.push(dict)
       }
 
-      console.log('questions: ', this.questions)
+      // console.log('questions: ', this.questions)
 
       // update total number of questions
       this.questionCounter = this.questions.length;
 
-      // allow to generate divs with questions - after all data is download from external api
+      // allow to generate view with questions - after all data is download from external api
       this.isStart = true;
 
       // create timer dictionary and fill with empty numbers
@@ -238,50 +246,95 @@ export default {
 
     },
     selectedAnswerFunction(event) {
-      // value of selected answer
+      /**
+       * Function save selected answer to a variable and adds it to dictionary.
+       * In addition, it updates progress bar.
+       * Function called when any answer under a given question is selected (click event).
+       */
+
+      // assigning content of answer to a variable
       this.selectedAnswer = event.target.innerText;
-      console.log(this.selectedAnswer = event.target.innerText)
+      // console.log(this.selectedAnswer = event.target.innerText)
+
       // add selected answer to dictionary depending on question index
       this.selectedAnswersDictionary[this.questionIndex] = this.selectedAnswer;
       // console.log(this.selectedAnswersDictionary)
+
       // update progress bar
       this.updateProgressBar()
     },
     hoverClassOn(event) {
+      /**
+       * Function changes class of answer container when mouse hovers it.
+       */
+
       this.isHovering = true
-      // console.log('hover on: ', this.isHovering)
       event.target.classList.add('option-hover')
     },
     hoverClassOff(event) {
+      /**
+       * Function changes class of answer container when mouse hovers it.
+       */
+
       this.isHovering = false
-      // console.log('hover off: ', this.isHovering)
       event.target.classList.remove('option-hover')
     },
     nextQuestion() {
+      /**
+       * Function to scroll through views with questions. Shows new question.
+       * Function called when next question button is pressed.
+       */
+
+      // increment question number
       this.questionIndex++;
-      this.selectedAnswer = '';
+
+      // reset selected answer
+      // this.selectedAnswer = '';
+
+      // save value from dictionary to selected answer
       this.selectedAnswer = this.selectedAnswersDictionary[this.questionIndex]
+
+      // show debug information in browser console
       this.info()
     },
     previousQuestion() {
+      /**
+       * Function to scroll through views with questions. Shows previous question.
+       * Function called when next question button is pressed.
+       */
+
+      // decrement question number
       this.questionIndex--;
-      this.selectedAnswer = '';
+
+      // reset selected answer
+      // this.selectedAnswer = '';
+
       // check what answer was selected in specific question depending on question index and answered question dictionary
       // based on this data - update selectedAnswer variable
-      // console.log('Actual question number= ', this.questionIndex)
       this.selectedAnswer = this.selectedAnswersDictionary[this.questionIndex]
-      // console.log('selectedAnswerForThisQuestion', this.selectedAnswer)
+
+      // show debug information in browser console
       this.info()
+
       // update progress bar
-      this.updateProgressBar()
+      // this.updateProgressBar()
     },
-    // Finish button
     showResults() {
+      /**
+       * Function to set variables before displaying results.
+       * Function called when finish button is pressed.
+       */
+
       // this.questionIndex++;
+
+      // variable to allows to generate results view
       this.isFinish = true;
-      console.log('RESULTS: ')
+
+      // show debug information in browser console
       this.info();
-      // compare correct answers with answers selected by user
+
+      // compare correct answers (questions dictionary) with answers selected by user (selectedAnswersDictionary dictionary)
+      // changing the counters of correct and wrong answers
       for (let i = 0; i < this.questions.length; i++) {
         if (this.questions[i].correctAnswer === this.selectedAnswersDictionary[i + 1]) {
           this.correctAnswers++
@@ -291,33 +344,52 @@ export default {
       }
       console.log('Correct answers = ', this.correctAnswers)
       console.log('Wrong answers = ', this.wrongAnswers)
+
       // calculate total time
-      // save actual time
+      // save actual global time
       this.timerGlobalStop = Date.now()
+
       // calculate difference between global timer start and stop and convert to seconds
       this.timerGlobalDelta = Math.floor((this.timerGlobalStop - this.timerGlobalStart) / 1000)
 
+      // set startGenerateChart to true - variable used in updated functions - indicates when charts can be generated
       this.startGenerateChart = true;
     },
     // static methods
     updateProgressBar() {
-      // console.log(Object.keys(this.selectedAnswersDictionary).length)
-      // update progress bar
+      /**
+       * Function to change width parameter in css style.
+       */
+
+      // variable used in 'progress status' view. Variable is based on length of dictionary of selected answers,
+      // not on actual question number (question index)
       this.widthProgress = (Object.keys(this.selectedAnswersDictionary).length / this.questionCounter) * 100;
-      console.log(this.widthProgress);
     },
     timer() {
-      // timer
+      /**
+       * Function to start and stop timer for specific question and save timer results to dictionary depending on
+       * actual question number (question index).
+       * Function called when next, previous, finish buttons are clicked.
+       */
+
+      // save actual time (like stop timer)
       this.actualTime = Date.now()
+
+      // calculate difference time between moment when new or previous question appears and clicked next or previous button
       this.timerDelta = this.actualTime - this.timerStart
+
       // save data to dictionary
       // add selected answer to dictionary depending on question index
       this.timerQuestionDictionary[this.questionIndex] += Math.floor(this.timerDelta / 1000)
-      // start timer
+
+      // save actual time (start timer)
       this.timerStart = Date.now()
     },
-    // function to randomize word position in correct answers array this.questions.correctAnswer
     shuffle(arr) {
+      /**
+       * Function to randomize word position in correct answers array this.questions.correctAnswer.
+       */
+
       for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -325,9 +397,16 @@ export default {
       return arr;
     },
     generateQuestionsResultsChart() {
+      /**
+       * Function to generate chart and update data (based on user results).
+       * Function shows ratio of correct to incorrect answers.
+       * Structure taken from the website 'chartjs.org'.
+       */
+
+          // find chart by id in html structure
       const ctx = document.getElementById('myChartQuestionsResults');
 
-      // chart
+      // chart object
       const myChartQuestionsResults = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -356,22 +435,28 @@ export default {
           }
         }
       });
-      // chart
+
+      // call chart object
       myChartQuestionsResults;
 
-
-      //  update chart
-      console.log('data chart before push', myChartQuestionsResults.data.datasets[0].data)
+      //  update chart - fill chart arrays with data
       myChartQuestionsResults.data.datasets[0].data.push(this.correctAnswers)  // correct answers
       myChartQuestionsResults.data.datasets[0].data.push(this.wrongAnswers)  // wrong answers
-      console.log('data chart after push', myChartQuestionsResults.data.datasets[0].data)
+
+      // update chart - without this, data will not be saved in object
       myChartQuestionsResults.update();
-      console.log('data chart after update', myChartQuestionsResults.data.datasets[0].data)
     },
     generateTimeResultsChart() {
+      /**
+       * Function to generate chart and update data (based on user results).
+       * Function shows time spent on each question.
+       * Structure taken from the website 'chartjs.org'.
+       */
+
+          // find chart by id in html structure
       const ctx = document.getElementById('myChartTimeResults');
 
-      // chart
+      // chart object
       const myChartTimeResults = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -394,14 +479,18 @@ export default {
           }
         }
       });
-      // chart
+
+      // call chart object
       myChartTimeResults;
 
+      // functions needed to generate random colors
       const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
       const randomByte = () => randomNumber(0, 255)
 
+      // save dictionary with amount of time spent for each question to local variable
       let dict = this.timerQuestionDictionary;
 
+      //  update chart - fill chart arrays with data
       Object.keys(dict).forEach(function (key) {
         myChartTimeResults.data.labels.push(`question ${key}`);  // data
         myChartTimeResults.data.datasets[0].data.push(dict[key]);  // labels
@@ -409,10 +498,26 @@ export default {
         myChartTimeResults.data.datasets[0].backgroundColor.push(color);  // push background color to array
         myChartTimeResults.data.datasets[0].borderColor.push(color);  // push border color to array
       })
+
+      // update chart - without this, data will not be saved in object
       myChartTimeResults.update()
+    },
+    info() {
+      /**
+       * Function to show actual information in browser console.
+       */
+
+      console.log('*********************************')
+      console.log('selectedAnswer = ', this.selectedAnswer)
+      console.log('selectedAnswersDictionary = ', this.selectedAnswersDictionary)
+      console.log('questionIndex = ', this.questionIndex)
+      console.log('*********************************')
     },
   },
   mounted() {
+    /**
+     * Function to show actual information in browser console when first view is loaded.
+     */
     console.log('*********************************')
     console.log('selectedAnswer = ', this.selectedAnswer)
     console.log('selectedAnswersDictionary = ', this.selectedAnswersDictionary)
@@ -420,7 +525,14 @@ export default {
     console.log('*********************************')
   },
   updated: function () {
+    /**
+     * Function that is called whenever a new view appears in the browser - when new DOM elements are loaded.
+     */
+
     this.$nextTick(function () {
+
+      // in order to prevent loading the charts after each change of view on the page, if function is used,
+      // so that the charts are loaded only after clicking Finish button and loading view with the results
       if (this.startGenerateChart === true) {
         this.generateQuestionsResultsChart();
         this.generateTimeResultsChart();
